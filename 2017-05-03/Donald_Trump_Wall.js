88bf25204d68fa5585f493bf6152873219f5227a
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-03 20:54:22 -0400

    Add 2017-05-3

---
+++ 2017-05-03/Donald_Trump_Wall.js
@@ -0,0 +1,16 @@
+var a = "do you want to build a wall? ";
+var b = "do you want to build the trump's wall? ";
+var c = "donald ttttttrrrrrrruuuummmmpppppp ";
+console.log(a + b + c);
+
+// donald trump
+var x = 1;
+var y = 1;
+var z = x + y;
+console.log(x + "+" + y + "=" + z);
+
+
+var q = "3";
+var w = "3";
+var e = q + w;
+console.log(w + "+" + q + "=" + e);
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 2017-04-25: Variables, increment, decrement, and console.log
 - 2017-05-01: Chocolate chip cookie everyday
 - 2017-05-02: Who is the best basketplayer?
+- 2017-05-03: Do you want to build a wall? 1+1=11 What?
