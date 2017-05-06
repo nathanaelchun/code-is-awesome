40ad8e85765e909043a697a5349f3b15871165f9
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-05 20:31:36 -0400

    Add 2017-05-05

---
+++ 2017-05-05/thank-God-it-is-friday.js
@@ -0,0 +1,6 @@
+var bn = "John";
+var c = "3";
+var v = "16";
+var m = "For God so loved the world that he gave his one and only son that whoever believes in him shall not perish but have everlasting life.";
+console.log(m);
+console.log("(" + bn + " " + c + ":" + v + ")");
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - 2017-05-02: Who is the best basketplayer?
 - 2017-05-03: Do you want to build a wall? 1+1=11 What?
 - 2017-05-04: I don't have time and NaN
+- 2017-05-05: Yay it is Friday and John 3:16
