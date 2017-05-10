d75f543eb8ec76b6a74e86745e424a20fcc80847
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-09 21:20:22 -0400

    Add another secret password

---
+++ 2017-05-09/secret_password.js
@@ -0,0 +1,11 @@
+var a = "peach"
+var brf = "oat meal"
+var dss = "bar of lard"
+var ds = "crazy animals"
+var vv = "archer"
+var ccccc = "super man"
+var u = "barf"
+var n = "please eat me"
+var nd = "are"
+var m = "run for your life"
+console.log(a[0] + brf[0] + dss[7] + ds[2] + vv[1] + ccccc[5] + u[0] + n[2] + nd[0] + m[0]);
