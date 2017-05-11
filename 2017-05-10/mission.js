988e2226b91b1ef38f52f08ad4255594cedb9854
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-10 20:11:53 -0400

    Add mission

---
+++ 2017-05-10/mission.js
@@ -0,0 +1,12 @@
+var m = "THE COMPLETE MIDDLE SCHOOL STUDY GUIDE EVERYTHING YOU NEED TO ACE AMERICAN HISTORY IN ONE BIG FAT NOTEBOOK.";
+console.log(m.slice(67, 69) + m.slice(9, 11));
+console.log(m.slice(3, 4));
+console.log(m.slice(67, 69));
+console.log(m.slice(12, 13));
+console.log(m.slice(95, 97));
+console.log(m.slice(97, 98));
+console.log(m.slice(0, 3));
+console.log(m.slice(3, 4));
+console.log(m.slice(102, 106));
+console.log(m.slice(26, 27));
+console.log(m.slice(77, 81) + m.slice(2, 3) + m.slice(-1));
