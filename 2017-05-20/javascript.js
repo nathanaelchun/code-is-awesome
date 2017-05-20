b2bc0d32766136abdb5d2c3987e991539b157845
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-20 09:53:42 -0400

    add 2017-05-20 javascript.js

--- 2017-05-20/javascript.js
+++ 2017-05-20/javascript.js
@@ -1,1 +1,12 @@
-var a="javascript is awesome!";
+var a = "javascript is awesome!";
+var b = 3 == 3;
+var c = "polar bears are awesome!!!";
+var d = 4 == 4;
+var t = "Are seals awesome?";
+var r = "not at all.";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(t);
+console.log(r);
