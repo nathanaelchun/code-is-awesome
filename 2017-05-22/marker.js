4ae714953f2bf2f5e510c990ef51c1aeea1e37de
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-22 21:43:06 -0400

    add 2017-05-22 marker.js

---
+++ 2017-05-22/marker.js
@@ -0,0 +1,11 @@
+console.log("hi");
+console.log("my name is marker.");
+console.log("I am a marker.");
+console.log("this is why i am called marker.");
+console.log("I live in marker city.");
+console.log("All markers are stupid.");
+console.log("Right now i am crossing the marksific ocean.");
+console.log("oh now I see new mark city.");
+console.log("oh now that is las markes.");
+console.log("(you):zzzzzzzzzzzzzzzzzzzzzzz.");
+
