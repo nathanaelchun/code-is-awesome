f2c8b09819f8168ab4bc2a7a16ea4a4bc2ab4e55
Nathanael Chun <nathanaelchun@gmail.com>
2017-05-24 19:25:56 -0400

    Add 2017-05-24 some message

---
+++ 2017-05-24/pop.js
@@ -0,0 +1,8 @@
+var ca = (9 == 10);
+console.log(ca && 5 == 6);
+console.log("or")
+var cat = (9 == 9);
+console.log(cat && 5 == 5);
+var n = "john";
+console.log(n == "mark" || "john" == n);
+console.log(n == "mark" || n == "john");
