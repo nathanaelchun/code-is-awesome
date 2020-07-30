d3d9c2b0eb3999f302b22a90b9cd1eee01071a73
Nathanael Chun <nathanaelchun@gmail.com>
2020-07-29 20:59:08 -0400

    add 2020-07-29

---
+++ 2020-07-29/Input_if.py
@@ -0,0 +1,8 @@
+answer = input("Enter a Random Number: ")
+x = int(answer)
+if x >5:
+    print("The number is greater than 5.")
+elif x ==5:
+    print("The number is equal to 5.")
+else:
+    print("The number is less than 5.")
