816a434832018fe92878c8789d0f9014f48d24fb
Nathanael Chun <nathanaelchun@gmail.com>
2020-08-01 21:43:21 -0500

    Rpg v1

---
+++ 2020-08-01/RPG_V1.py
@@ -0,0 +1,33 @@
+option_1 = "1"
+option_2 = "2"
+option_3 = "3"
+option_4 = "4"
+option_1b = "1"
+option_2b = "2"
+option_3b = "3"
+option_4b = "4"
+option_1c = "1"
+option_2c = "2"
+option_3c = "3"
+option_4c = "4"
+option_1_answer ="5"
+option_2_answer ="6"
+option_3_answer ="7"
+option_4_answer ="8"
+option_1_answer_b ="5"
+option_2_answer_b ="6"
+option_3_answer_b ="7"
+option_4_answer_b ="8"
+option_1_answer_c ="5"
+option_2_answer_c ="6"
+option_3_answer_c ="7"
+option_4_answer ="8"
+answer_1 = input(option_1+","+option_2+","+option_3+","+option_4+",")
+if answer_1 == option_1:
+    print(option_1_answer)
+if answer_1 == option_2:
+    print(option_2_answer)
+if answer_1 == option_3:
+    print(option_3_answer)
+if answer_1 == option_4:
+    print(option_4_answer)
